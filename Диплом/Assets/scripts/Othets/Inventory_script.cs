28e4ed97fc2567c450b0e87f6f262f6309cc72f1
Chepinoga1 <96283348+Wistrekz@users.noreply.github.com>
2023-02-07 15:21:49 +0300

    ghv

--- Диплом/Assets/scripts/Othets/Inventory_script.cs
+++ Диплом/Assets/scripts/Othets/Inventory_script.cs
@@ -7,25 +7,70 @@ public class Inventory_script : MonoBehaviour
     public GameObject Inventory_Panel;
     public GameObject Inventory_Pick;
     public GameObject Info_Panel;
+    public moving Mover;
     public string path;
+    public float movingInInventory;
+    public GameObject ItemName, ItemInfo;
+    public GameObject[] Item_places;
 
     public static bool Inventory_open;
 
+    private int numberOfInventory;
+
     private void Update()
     {
-        if(Input.GetButton("Fire3") || Input.GetButton("Fire4"))
+        if(Input.GetButtonDown("Fire3") || Input.GetButton("Fire4"))
         {
             if(!script_for_Events.Cutscenegoing)
-                Inventory_open = true;
+                Inventory_open = !Inventory_open;
         }
         if(Inventory_open)
         {
             Inventory_Panel.SetActive(true);
             Inventory_Pick.SetActive(true);
+            Mover.StopMoving();
+            for(int i = 0; i < Inventory_storage.Player_inventory.Length; i++)
+            {
+
+            }
         }
         else
         {
             Inventory_Panel.SetActive(false);
+            Mover.StartMoving();
+        }
+        if (Input.GetButton("Left") || Input.GetButton("Right"))
+        {
+            if(Input.GetButtonDown("Left"))
+            {
+                if(numberOfInventory > 0)
+                {
+                    Inventory_Pick.transform.position = new Vector2(Inventory_Pick.transform.position.x - movingInInventory, Inventory_Pick.transform.position.y);
+                    numberOfInventory--;
+                }
+            }
+            if(Input.GetButtonDown("Right"))
+            {
+                if(numberOfInventory < 5)
+                {
+                    Inventory_Pick.transform.position = new Vector2(Inventory_Pick.transform.position.x + movingInInventory, Inventory_Pick.transform.position.y);
+                    numberOfInventory++;
+                }
+            }
+        }
+        if(Inventory_open && (Input.GetButtonDown("Fire1") || Input.GetButtonDown("Fire2")))
+        {
+            if(Inventory_storage.Player_inventory != null)
+            {
+                for (int i = 0; i < ; i++)
+                {
+                    
+                }
+            }
+            else
+            {
+
+            }
         }
     }
 }
---
+++ Диплом/Assets/scripts/Player's things/Inventory_storage.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory_storage : MonoBehaviour
+{
+    public int count_Items;
+    public string pathToInfo;
+
+    public static string PathInformation;
+
+    public static Inventory_Storage[] Player_inventory;
+
+    [System.Serializable]
+    public struct Inventory_Storage
+    {
+        public string name;
+        public string pathtoSprite;
+    }
+    public List<Inventory_Storage> Items_Storage;
+    public static List<Inventory_Storage> _Inventory_Storage;
+
+    void Start()
+    {
+        _Inventory_Storage = Items_Storage;
+        PathInformation = Dictionary_files.Mark_Reader(pathToInfo);
+    }
+}
